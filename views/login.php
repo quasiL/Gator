1e924e22d2eeb841360e2862f70687a7623c1ba4
Artur Khairullin <khairart@fel.cvut.cz>
2023-09-23 16:24:41 +0200

    wrong tag

--- views/login.php
+++ views/login.php
@@ -4,9 +4,9 @@ use app\models\User;
 ?>
 <h1>Login</h1>
 <?php $form = \app\src\form\Form::begin('', 'post'); ?>
-<lablel>email</lablel>
+<label>email</label>
 <?php echo $form->field($model, 'email'); ?>
-<lablel>password</lablel>
+<label>password</label>
 <?php echo $form->field($model, 'password')->passwordField(); ?>
 <button type="submit">Login</button>
 <?php \app\src\form\Form::end(); ?>
